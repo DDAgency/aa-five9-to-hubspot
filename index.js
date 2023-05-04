0ed6bcec3807a63fad2706e5ce8e53a036de45c7
Matthew Fall <matthew@MacBook-Pro-2.local>
2023-05-04 07:45:15 -0600

    debug;

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(nocache());
 /* Allows app to handle JSON POST data */
 app.use(express.json());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.post('/', (req, res) => {
@@ -52,61 +52,59 @@ app.post('/', (req, res) => {
             json: true,
         };
 
+        console.log(options);
+            res.status(200).end();
 
-        request(options, function(error, response, body) {
-            if (error) throw new Error(error);
-            res.json({
-                received: true
-            });
-        });
+
+      //   request(options, function(error, response, body) {
+      //       if (error) throw new Error(error);
+      // res.status(200).end();
+      //   });
     }
 
 
-    console.log(req.body);
-    console.log(req.params);
-    console.log(req.query);
-
-res.status(200).end();
-
-    // const data = req.body;
-
-    // const recordId = req.body['Record Id - Contact'] ? req.body['Record Id - Contact'] : false;
-    // const recordEmail = req.body.email ? req.body.email : false;
-
-    // const properties = {
-    //     'firstname': data.first_name,
-    //     'lastname': data.last_name,
-    //     'phone': data.phone1,
-    //     'phone_number_2': data.phone2,
-    //     'phone_number_3': data.phone3,
-    //     'email': data.email,
-    //     'district': data['District'],
-    //     'date': data['Date of Birth'],
-    //     'address': data.street,
-    //     'street_address_2': data['Street Address 2'],
-    //     'zip': data.zip,
-    //     'city': data.city,
-    //     'state': data.state,
-    //     'auto_dialer_create_date': data['Create Date'],
-    //     'direction_of_call': data['Direction of Call'],
-    //     'notes_last_updated': data['Last Activity Date'],
-    //     'last_agent': data['Last Agent'],
-    //     'time_or_date_of_call': data['Time or Date of Call'],
-    //     'time_or_length_of_call': data['Time or Length of Call'],
-    //     'last_disposition': data.disposition_name
-    // }
 
 
-    // if (recordId) {
-    //     updateContact('recordId', recordId, properties);
-    // } else if (recordEmail) {
-    //     updateContact('email', recordEmail, properties);
-    // } else {
-    //     res.json({
-    //        'error': 'No unique identifier found'
-    //     });
 
-    // }
+    const data = req.body;
+
+    const recordId = req.body['Record Id - Contact'] ? req.body['Record Id - Contact'] : false;
+    const recordEmail = req.body.email ? req.body.email : false;
+
+    const properties = {
+        'firstname': data.first_name,
+        'lastname': data.last_name,
+        'phone': data.phone1,
+        'phone_number_2': data.phone2,
+        'phone_number_3': data.phone3,
+        'email': data.email,
+        'district': data['District'],
+        'date': data['Date of Birth'],
+        'address': data.street,
+        'street_address_2': data['Street Address 2'],
+        'zip': data.zip,
+        'city': data.city,
+        'state': data.state,
+        'auto_dialer_create_date': data['Create Date'],
+        'direction_of_call': data['Direction of Call'],
+        'notes_last_updated': data['Last Activity Date'],
+        'last_agent': data['Last Agent'],
+        'time_or_date_of_call': data['Time or Date of Call'],
+        'time_or_length_of_call': data['Time or Length of Call'],
+        'last_disposition': data.disposition_name
+    }
+
+
+    if (recordId) {
+        updateContact('recordId', recordId, properties);
+    } else if (recordEmail) {
+        updateContact('email', recordEmail, properties);
+    } else {
+        res.json({
+           'error': 'No unique identifier found'
+        });
+
+    }
 
 
 });
