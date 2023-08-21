52da8a656e5e6a440a18b05f316f3b5630b3716b
Matthew Fall <matthew@MacBook-Pro-2.local>
2023-08-21 13:18:25 -0600

    syntax

--- index.js
+++ index.js
@@ -137,6 +137,8 @@ app.post('/', (req, res) => {
         'Yaney Hatfield': 202881385 
     };
 
+    // https://api.hubapi.com/calling/v1/dispositions
+
     const callOutcomes = {
         "Abandon": "d5d2a784-4f85-4927-85da-20b8a5612145",
         "Aged Out": "c4a4bdf6-ddc0-4765-acf8-c366aae58e64",
@@ -153,7 +155,7 @@ app.post('/', (req, res) => {
         "Moved out of area": "6408fc1f-3e5d-4283-9bf6-ecb2a2409300",
         "No Answer": "887abc28-1024-4b1c-af9d-20cfb41432ef",
         "No/Full VM + text. ": "1c5daf0b-0edf-4ec2-b9e4-ba05bcad11f7",
-        "Registration Link Sent": "68a60c26-e1b3-4b0b-bd90-b1de0598182c"
+        "Registration Link Sent": "68a60c26-e1b3-4b0b-bd90-b1de0598182c",
         "Spanish Callback Needed": "24a2c8ff-a391-498e-b1aa-b7de502fece1",
         "Underage" "d8d3007a-e092-4ae4-af58-84527ac4ba62"
     };
